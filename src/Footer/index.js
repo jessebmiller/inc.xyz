cd7fe0a28659d104b925b884d1be9402d7c1fcc8
Jesse B. Miller <jesse@jessebmiller.com>
2018-04-28 21:17:11 -0500

    small (get it?) update to footer

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -1,9 +1,7 @@
 import React from 'react'
 
-class Footer extends React.Component {
-  render() {
-    return <h1>Footer Placeholder</h1>
-  }
+const Footer = () => {
+  return <footer>Footer placeholder</footer>
 }
 
 export default Footer
