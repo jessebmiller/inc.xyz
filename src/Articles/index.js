999a72a85acdc8f03609cb2d2f8f3e6a16e1f99e
Jesse B. Miller <jesse@jessebmiller.com>
2018-05-06 15:22:37 -0500

    lifecycle messages for articles

--- src/Articles/index.js
+++ src/Articles/index.js
@@ -1,21 +1,41 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-let Articles = (state) => {
-  return (
+let Articles = ({ articles, lifecycleState }) => {
+  switch(lifecycleState) {
+  case "REQUESTED":
+    return (
+      <p>Requested articles...</p>
+    )
+  case "RECIEVED":
+    return (
     <ul>
-      {state.articles.map((article) => {
+      {articles.map((article) => {
         return (
           <p key={Math.random()}>article list entry placeholder {article}</p>
         )
       })}
     </ul>
   )
+  case "FETCH_ERROR":
+    return (
+      <p>Error fetching articles :/ sorry this is new..</p>
+    )
+  case "INITIAL":
+    return (
+      <p>We still need to load articles when you refresh on this URL...</p>
+    )
+  default:
+    return (
+      <p>We don't know where the articles are...sorry this is new.</p>
+    )
+  }
 }
 
 function mapState(state) {
   let mapping = {
-    articles: state.articles.resources
+    articles: state.articles.resources,
+    lifecycleState: state.articles.lifecycleState
   }
   return mapping
 }
